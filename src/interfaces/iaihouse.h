77ebb9cc5c2b520b956d11cf850c0b499dcdd023
CCHyper <73803386+CCHyper@users.noreply.github.com>
2021-03-31 22:44:22 +0100

    Adds IAIHouse.

---
+++ src/interfaces/iaihouse.h
@@ -0,0 +1,51 @@
+/*******************************************************************************
+/*                     O P E N  S O U R C E  --  T S + +                      **
+/*******************************************************************************
+ *
+ *  @project       TS++
+ *
+ *  @file          IAIHOUSE.H
+ *
+ *  @authors       CCHyper
+ *
+ *  @brief         IAIHouse interface.
+ *
+ *  @license       TS++ is free software: you can redistribute it and/or
+ *                 modify it under the terms of the GNU General Public License
+ *                 as published by the Free Software Foundation, either version
+ *                 3 of the License, or (at your option) any later version.
+ *
+ *                 TS++ is distributed in the hope that it will be
+ *                 useful, but WITHOUT ANY WARRANTY; without even the implied
+ *                 warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *                 PURPOSE. See the GNU General Public License for more details.
+ *
+ *                 You should have received a copy of the GNU General Public
+ *                 License along with this program.
+ *                 If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+#pragma once
+
+#include "always.h"
+#include "ihouse.h"
+#include <unknwn.h>
+
+
+/*******************************************************************************
+ *  @class   IAIHouse
+ *
+ *  @brief   AI for House.
+ */
+DECLARE_INTERFACE_IID_(IAIHouse, IUnknown, "96F02EC4-6FE8-11D1-B6FD-00A024DDAFD1")
+{   
+    /**
+     *  Links house to an advisor object.
+     */
+    STDMETHOD_(LONG, Link_House)(IHouse *house) PURE;
+
+    /**
+     *  AI processing entry function.
+     */
+    STDMETHOD_(LONG, AI)(long *framedelay) PURE;
+};
