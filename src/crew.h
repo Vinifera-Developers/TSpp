1b351204bef905a36eb8c4122a509fafe69e6604
CCHyper <73803386+CCHyper@users.noreply.github.com>
2021-04-03 00:04:23 +0100

    Adds CrewClass.

---
+++ src/crew.h
@@ -0,0 +1,51 @@
+/*******************************************************************************
+/*                     O P E N  S O U R C E  --  T S + +                      **
+/*******************************************************************************
+ *
+ *  @project       TS++
+ *
+ *  @file          CREW.H
+ *
+ *  @authors       CCHyper
+ *
+ *  @brief         Unit crew and kill tracker.
+ *
+ *  @license       TS++ is free software: you can redistribute it and/or
+ *                 modify it under the terms of the GNU General Public License
+ *                 as published by the Free Software Foundation, either version
+ *                 3 of the License, or (at your option) any later version.
+ *
+ *                 TS++ is distributed in the hope that it will be
+ *                 useful, but WITHOUT ANY WARRANTY; without even the implied
+ *                 warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *                 PURPOSE. See the GNU General Public License for more details.
+ *
+ *                 You should have received a copy of the GNU General Public
+ *                 License along with this program.
+ *                 If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+#pragma once
+
+#include "always.h"
+
+
+class NoInitClass;
+
+
+class CrewClass
+{
+    public:
+        CrewClass() {}
+        CrewClass(const NoInitClass &noinit) {}
+        ~CrewClass() {}
+
+		int Made_A_Kill()
+		{
+			Kills++;
+			return Kills;
+		};
+
+    public:
+		unsigned short Kills;
+};
