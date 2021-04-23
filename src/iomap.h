fe3468ea593104cf7c460aa624ffbf30c59280c5
CCHyper <73803386+CCHyper@users.noreply.github.com>
2021-04-23 10:39:32 +0100

    Adds IOMap header.

---
+++ src/iomap.h
@@ -0,0 +1,37 @@
+/*******************************************************************************
+/*                     O P E N  S O U R C E  --  T S + +                      **
+/*******************************************************************************
+ *
+ *  @project       TS++
+ *
+ *  @file          IOMAP.H
+ *
+ *  @authors       CCHyper
+ *
+ *  @brief         Provides access to the map IO interface.
+ *
+ *  @license       TS++ is free software: you can redistribute it and/or
+ *                 modify it under the terms of the GNU General Public License
+ *                 as published by the Free Software Foundation, either version
+ *                 3 of the License, or (at your option) any later version.
+ *
+ *                 TS++ is distributed in the hope that it will be
+ *                 useful, but WITHOUT ANY WARRANTY; without even the implied
+ *                 warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *                 PURPOSE. See the GNU General Public License for more details.
+ *
+ *                 You should have received a copy of the GNU General Public
+ *                 License along with this program.
+ *                 If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+#pragma once
+
+#include "tibsun_globals.h"
+#include "mouse.h"
+
+
+/**
+ *  This is a simple utility header to provide access to the "Map" global
+ *  and include any headers required to access it and its classes.
+ */
