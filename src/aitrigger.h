dd85e10dc8b9a10838498e742b5cf5c7c2cb6dda
CCHyper <73803386+CCHyper@users.noreply.github.com>
2021-04-25 22:08:53 +0100

    Adds AITriggerClass.

---
+++ src/aitrigger.h
@@ -0,0 +1,52 @@
+/*******************************************************************************
+/*                     O P E N  S O U R C E  --  T S + +                      **
+/*******************************************************************************
+ *
+ *  @project       TS++
+ *
+ *  @file          AITRIGGER.H
+ *
+ *  @authors       CCHyper
+ *
+ *  @brief         AI trigger object class.
+ *
+ *  @license       TS++ is free software: you can redistribute it and/or
+ *                 modify it under the terms of the GNU General Public License
+ *                 as published by the Free Software Foundation, either version
+ *                 3 of the License, or (at your option) any later version.
+ *
+ *                 TS++ is distributed in the hope that it will be
+ *                 useful, but WITHOUT ANY WARRANTY; without even the implied
+ *                 warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *                 PURPOSE. See the GNU General Public License for more details.
+ *
+ *                 You should have received a copy of the GNU General Public
+ *                 License along with this program.
+ *                 If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+#pragma once
+
+#include "object.h"
+
+
+class DECLSPEC_UUID("03C4CE76-4CF5-11D2-BC26-00104B8FB04D")
+AITriggerClass : public AbstractClass
+{
+    public:
+        /**
+         *  IPersist
+         */
+        IFACEMETHOD(GetClassID)(CLSID *pClassID);
+
+        /**
+         *  IPersistStream
+         */
+        IFACEMETHOD(Load)(IStream *pStm);
+        IFACEMETHOD(Save)(IStream *pStm, BOOL fClearDirty);
+
+    public:
+        AITriggerClass();
+        AITriggerClass(NoInitClass &noinit);
+        virtual ~AITriggerClass();
+};
