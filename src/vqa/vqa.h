fa8c48a2bd26f7502c9343c30dcae7981e100451
CCHyper <73803386+CCHyper@users.noreply.github.com>
2021-12-23 17:06:19 +0000

    Adds initial VQA structs and library functions.

--- src/vqa/vqa.h
+++ src/vqa/vqa.h
@@ -28,10 +28,34 @@
 #pragma once
 
 #include "always.h"
+#include "dsaudio.h"
 
 
-struct VQAConfig { char pad[0x88]; };
-struct VQAHandle { char pad[0x7DC]; };
+class VQAClass;
+
+
+/**
+ *  We need to redefine these as DSAudio uses pointers and here we use instances.
+ */
+typedef struct tWAVEFORMATEX_6
+{
+    WORD wFormatTag;
+    WORD nChannels;
+    DWORD nSamplesPerSec;
+    DWORD nAvgBytesPerSec;
+    WORD nBlockAlign;
+    WORD wBitsPerSample;
+    WORD cbSize;
+} WAVEFORMATEX_6, *PWAVEFORMATEX_6, NEAR *NPWAVEFORMATEX_6, FAR *LPWAVEFORMATEX_6;
+
+typedef struct _DSBUFFERDESC_6
+{
+    DWORD dwSize;
+    DWORD dwFlags;
+    DWORD dwBufferBytes;
+    DWORD dwReserved;
+    LPWAVEFORMATEX_6 lpwfxFormat;
+} DSBUFFERDESC_6, *LPDSBUFFERDESC_6;
 
 
 typedef enum VQAErrorType
@@ -89,6 +113,285 @@ typedef enum VQAOptionType
 } VQAOptionType;
 
 
+// NEEDS CONFIRMING
+#pragma pack(push, 1)
+typedef struct VQAHeader
+{
+    unsigned short Version;
+    unsigned short Flags;
+    unsigned short Frames;
+    unsigned short ImageWidth;
+    unsigned short ImageHeight;
+    unsigned char BlockWidth;
+    unsigned char BlockHeight;    
+    unsigned char FPS;
+    unsigned char GroupSize;
+    unsigned short Num1Colors;
+    unsigned short CBEntries;
+    unsigned short XPos;
+    unsigned short YPos;
+    unsigned short MaxFrameSize;
+    unsigned short SampleRate;
+    unsigned char Channels;
+    unsigned char BitsPerSample;
+    unsigned char AltSampleRate;
+    unsigned char AltChannels;
+    unsigned char AltBitsPerSample;
+    unsigned char ColorMode;
+    unsigned char field_21;
+    unsigned long MaxCBFZSize;
+    unsigned long field_26;
+} VQAHeader;
+#pragma pack(pop)
+
+
+// MATCHES SIZE
+typedef struct VQAConfig
+{
+    void *StreamHandler;
+    void *MemoryHandler;
+    void *PaletteHandler;
+    void *DrawerCallback;
+    void *TimerHandler;
+    long field_14;
+    long AudioHandler;
+    unsigned char *ImageBuf;
+    long ImageWidth;
+    long ImageHeight;
+    long X1;
+    long Y1;
+    long FrameRate;
+    long DrawRate;
+    long RefreshRate;
+    long TimerMethod;
+    long DrawFlags;
+    long OptionFlags;
+    long NumFrameBufs;
+    long NumCBBufs;
+    unsigned char *VocFile;
+    unsigned char *AudioBuf;
+    long AudioBufSize;
+    long HMIBufSize;
+    long AudioRate;
+    long Volume;
+    long field_68;
+    long field_6C;
+    long field_70;
+    long field_74;
+    long DigiHandle;
+    long field_7C;
+    long field_80;
+    long LatencyAdjustment;
+} VQAConfig;
+
+
+// CONFIRMED
+typedef struct VQAInfo
+{
+    long NumFrames;
+    long ImageWidth;
+    long ImageHeight;
+} VQAInfo;
+
+
+// CONFIRMED
+typedef struct VQAChunkHeader
+{
+    unsigned long ID;
+    unsigned long Size;
+} VQAChunkHeader;
+
+
+// CONFIRMED
+typedef struct VQACacheHeader
+{
+    unsigned long FileOffset;
+    unsigned long FileSize;
+} VQACacheHeader;
+
+
+// CONFIRMED
+typedef struct VQACBNode
+{
+    unsigned char *Buffer;
+    VQACBNode *Next;
+    VQACBNode *Prev;
+    unsigned long Flags;
+    unsigned long CBOffset;
+    unsigned long Size;
+} VQACBNode;
+
+
+// CONFIRMED
+typedef struct VQAFrameNode
+{
+    unsigned char *Polongers;
+    VQACBNode *Codebook;
+    unsigned char *Palette;
+    VQAFrameNode *Next;
+    VQAFrameNode *Prev;
+    unsigned long Flags;
+    unsigned long PrevFlags;
+    long FrameNum;
+    long PtrOffset;
+    long PalOffset;
+    long PaletteSize;
+} VQAFrameNode;
+
+
+typedef struct VQALoader
+{
+    VQACBNode *CurCB;
+    VQACBNode *FullCB;
+long __CurCodebook;                    // Whats this?
+    VQAFrameNode *CurFrame;
+    long NumPartialCB;
+    long PartialCBSize;
+    long CurFrameNum;            // __PartialCBSizeMax?
+    long LastCBFrame;            // __NextFrameNum?
+    long LastFrameNum;
+    long WaitsOnDrawer;
+    long WaitsOnAudio;
+    long FrameSize;
+    long MaxFrameSize;
+} VQALoader;
+
+
+typedef struct VQAStatistics
+{
+    long StartTime;
+    long EndTime;
+    long FramesLoaded;
+    long FramesDrawn;
+    long FramesSkipped;
+    long MaxFrameSize;
+    unsigned long SamplesPlayed;
+    unsigned long MemUsed;
+} VQAStatistics;
+
+
+// NEEDS CONFIRMING
+typedef struct VQAAudio
+{
+    unsigned char *Buffer;
+    unsigned long AudBufPos;
+    short *IsLoaded;
+    unsigned long NumAudBlocks;
+
+
+
+    unsigned char *TempBuf;
+    unsigned long TempBufSize;
+    unsigned long TempBufLen;
+    unsigned long Flags;
+    unsigned long PlayPosition;
+    unsigned long SamplesPlayed;
+    unsigned long NumSkipped;
+    unsigned short SampleRate;
+    unsigned char Channels;
+    unsigned char BitsPerSample;
+
+    _tagCOMPRESS_INFO2 sSOSInfo;
+    unsigned long SoundTimerHandle;    
+
+    //TODO
+
+    WAVEFORMATEX_6 WaveFormat;
+    DSBUFFERDESC_6 BufferDesc;
+
+    long field_AC;
+    long field_B0;
+    long field_B4;
+    long field_C8;
+    long field_BC;
+    long field_C0;
+} VQAAudio;
+
+
+// CONFIRMED
+typedef struct VQAClipper
+{
+    unsigned long Width;
+    unsigned long Height;
+} VQAClipper;
+
+
+// CONFIRMED
+typedef struct VQAFlipper
+{
+    VQAFrameNode *CurFrame;
+    long LastFrameNum;
+} VQAFlipper;
+
+
+typedef struct VQADrawer
+{
+    VQAFrameNode *CurFrame;
+    unsigned long Flags;
+    unsigned char *ImageBuf;
+    long ImageWidth;
+    long ImageHeight;
+    long X1;
+    long Y1;
+//    long X2;
+//    long Y2;
+    long ScreenOffset;
+    long CurPalSize;
+    char Palette_24[768];
+    char Palette_15[512];
+    long BlocksPerRow;
+    long NumRows;
+    long NumBlocks;
+//    long MaskStart;            // Where did these come from?
+//    long MaskWidth;
+//    long MaskHeight;
+//    long LastTime;             // Where did these come from?
+//    long LastFrame;
+//    long LastFrameNum;
+    long DesiredFrame;
+//    long NumSkipped;           // ?
+    long WaitsOnFlipper;
+    long WaitsOnLoader;
+} VQADrawer;
+
+
+typedef struct VQAData
+{
+    void *Draw_Frame;
+    void *Page_Flip;
+    void *UnVQ;
+    VQAFrameNode *FrameData;
+    VQACBNode *CBData;
+    VQAAudio Audio;
+    VQALoader Loader;
+    VQADrawer Drawer;
+    VQAFlipper Flipper;
+    unsigned long Flags;
+    long *Foff;
+    long VBIBit;
+    long Max_CB_Size;
+    long Max_Pal_Size;
+    long Max_Ptr_Size;
+    long LoadedFrames;
+    long DrawnFrames;
+    long StartTime;
+    long EndTime;
+    long MemUsed;
+} VQAData;
+
+
+typedef struct VQAHandle
+{
+    unsigned long VQAStream;
+    void *StreamHandler;
+    VQAData *VQABuf;
+    VQAConfig Config;
+    VQAClass *Class;
+    VQAHeader Header;
+    long vocfh;
+} VQAHandle;
+
+
 class VQAClass
 {
     public:
@@ -125,7 +428,7 @@ class VQAClass
         bool field_64;
         char Filename[263];
         VQAConfig Config;
-        void * Handle;
+        VQAHandle *Handle;
         char Palette[768];
         int CurrentFrame;
         int TotalFrames;
@@ -150,8 +453,15 @@ class VQAClass
         bool field_545;
 };
 
+
+/**
+ *  VQA library functions.
+ */
+
 void VQA_Init_Option();
 void VQA_Set_Option(VQAOptionType option);
 void VQA_Clear_Option(VQAOptionType option);
 bool VQA_Get_Option(VQAOptionType option);
 bool VQA_Flip_Option(VQAOptionType option);
+
+unsigned long VQA_GetTime(VQAHandle *handle);
