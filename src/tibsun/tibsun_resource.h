dc8f0f076d96113f65f91240e248248b59ade588
CCHyper <73803386+CCHyper@users.noreply.github.com>
2023-02-14 04:03:13 +0000

    Initial resources header.

---
+++ src/tibsun/tibsun_resource.h
@@ -0,0 +1,59 @@
+/*******************************************************************************
+/*                     O P E N  S O U R C E  --  T S + +                      **
+/*******************************************************************************
+ *
+ *  @project       TS++
+ *
+ *  @file          TIBSUN_RESOURCE.H
+ *
+ *  @authors       CCHyper
+ *
+ *  @brief         Resource defines and constant.
+ *
+ *  @license       TS++ is free software: you can redistribute it and/or
+ *                 modify it under the terms of the GNU General Public License
+ *                 as published by the Free Software Foundation, either version
+ *                 3 of the License, or (at your option) any later version.
+ *
+ *                 TS++ is distributed in the hope that it will be
+ *                 useful, but WITHOUT ANY WARRANTY; without even the implied
+ *                 warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *                 PURPOSE. See the GNU General Public License for more details.
+ *
+ *                 You should have received a copy of the GNU General Public
+ *                 License along with this program.
+ *                 If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+#pragma once
+
+#include "always.h"
+
+
+/**
+ *  Icons
+ */
+#define IDI_TIBSUN              93
+#define IDI_CHECKBOX_ON         160
+#define IDI_CHECKBOX_OFF        161
+#define IDI_CHECKBOX_DISABLED   162
+
+
+/**
+ *  Cursors
+ */
+#define IDC_TIBSUN_ARROW        104
+#define IDC_TIBSUN_GREEN_ARROW  248
+
+
+/**
+ *  Strings
+ */
+//#define IDS
+// See language.h
+
+
+/**
+ *  Menus
+ */
+#define IDM_RESOLUTION          160
