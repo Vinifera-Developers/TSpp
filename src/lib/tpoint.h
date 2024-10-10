ca279e1b531a152ff836cc419be069eca5a2874f
ZivDero <kirill.andriiashin@gmail.com>
2024-10-10 23:43:53 +0300

    Fix TPoint2D and TPoint3D

--- src/lib/tpoint.h
+++ src/lib/tpoint.h
@@ -25,137 +25,107 @@
 template<typename T>
 class TPoint2D
 {
-    public:
-        TPoint2D() : X(0), Y(0) {}
-        TPoint2D(T x, T y) : X(x), Y(y) {}
-        TPoint2D(const TPoint2D &that) : X(that.X), Y(that.Y) {}
+public:
+    TPoint2D() : X(0), Y(0) {}
+    TPoint2D(T x, T y) : X(x), Y(y) {}
+    TPoint2D(const TPoint2D &that) : X(that.X), Y(that.Y) {}
 
-        TPoint2D &operator=(const TPoint2D &that)
-        {
-            if ( this != &that ) {
-                X = that.X;
-                Y = that.Y;
-            }
-            return *this;
-        }
+    TPoint2D &operator=(const TPoint2D &that) {
+        X = that.X;
+        Y = that.Y;
+        return *this;
+    }
 
-        bool operator==(const TPoint2D &that) const { return X == that.X && Y == that.Y; }
-        bool operator!=(const TPoint2D &that) const { return X != that.X && Y != that.Y; }
+    bool operator==(const TPoint2D &that) const { return X == that.X && Y == that.Y; }
+    bool operator!=(const TPoint2D &that) const { return X != that.X && Y != that.Y; }
 
-        bool Is_Valid() const { return X > 0 || Y > 0; }
+    TPoint2D &operator+=(const TPoint2D &that)
+    {
+        X += that.X;
+        Y += that.Y;
+        return *this;
+    }
 
-    public:
-        T X;
-        T Y;
+    TPoint2D& operator-=(const TPoint2D& that) {
+        X -= that.X;
+        Y -= that.Y;
+        return *this;
+    }
+
+    TPoint2D operator+(const TPoint2D &that) const {
+        TPoint2D result = *this;
+        result += that;
+        return result;
+    }
+
+    TPoint2D operator-(const TPoint2D& that) const {
+        TPoint2D result = *this;
+        result -= that;
+        return result;
+    }
+     
+    TPoint2D operator-() const { return TPoint2D(-X, -Y); }
+    TPoint2D operator+() const { return TPoint2D(X, Y); }
+
+    bool Is_Valid() const { return X > 0 || Y > 0; }
+
+public:
+    T X;
+    T Y;
 };
-
-
-template<typename T>
-bool operator==(const TPoint2D<T> &left, const TPoint2D<T> &right)
-{
-    return (left.X == right.X)
-        && (left.Width == right.Width)
-        && (left.Y == right.Y)
-        && (left.Height == right.Height);
-}
-
-
-template<typename T>
-bool operator!=(const TPoint2D<T> &left, const TPoint2D<T> &right)
-{
-    return (left.X != right.X)
-        && (left.Width != right.Width)
-        && (left.Y != right.Y)
-        && (left.Height != right.Height);
-}
-
-
-template<typename T>
-TPoint2D<T> operator+(const TPoint2D<T> &left, const TPoint2D<T> &right)
-{
-    Rect result = left;
-    Rect tmp = right;
-    result += tmp;
-    return result;
-}
-
-
-template<typename T>
-TPoint2D<T> operator-(const TPoint2D<T> &left, const TPoint2D<T> &right)
-{
-    Rect result = left;
-    Rect tmp = right;
-    result -= tmp;
-    return result;
-}
-
 
 
 template<typename T>
 class TPoint3D
 {
-    public:
-        TPoint3D() : X(0), Y(0), Z(0) {}
-        TPoint3D(T x, T y, T z) : X(x), Y(y), Z(z) {}
-        TPoint3D(const TPoint3D &that) : X(that.X), Y(that.Y), Z(that.Z) {}
+public:
+    TPoint3D() : X(0), Y(0), Z(0) {}
+    TPoint3D(T x, T y, T z) : X(x), Y(y), Z(z) {}
+    TPoint3D(const TPoint3D &that) : X(that.X), Y(that.Y), Z(that.Z) {}
 
-        TPoint3D &operator=(const TPoint3D &that)
-        {
-            if ( this != &that ) {
-                X = that.X;
-                Y = that.Y;
-                Z = that.Z;
-            }
-            return *this;
-        }
+    TPoint3D &operator=(const TPoint3D &that) {
+        X = that.X;
+        Y = that.Y;
+        Z = that.Z;
+        return *this;
+    }
 
-        bool operator==(const TPoint3D &that) const { return X == that.X && Y == that.Y && Z == that.Z; }
-        bool operator!=(const TPoint3D &that) const { return X != that.X && Y != that.Y && Z != that.Z; }
+    bool operator==(const TPoint3D &that) const { return X == that.X && Y == that.Y && Z == that.Z; }
+    bool operator!=(const TPoint3D &that) const { return X != that.X && Y != that.Y && Z != that.Z; }
 
-        bool Is_Valid() const { return X > 0 || Y > 0; }
+    TPoint3D& operator+=(const TPoint3D& that) {
+        X += that.X;
+        Y += that.Y;
+        Z += that.Z;
+        return *this;
+    }
 
-    public:
-        T X;
-        T Y;
-        T Z;
+    TPoint3D& operator-=(const TPoint3D& that) {
+        X -= that.X;
+        Y -= that.Y;
+        Z -= that.Z;
+        return *this;
+    }
+
+    TPoint3D operator+(const TPoint3D& that) const {
+        TPoint3D result = *this;
+        result += that;
+        return result;
+    }
+
+    TPoint3D operator-(const TPoint3D& that) const {
+        TPoint3D result = *this;
+        result -= that;
+        return result;
+    }
+
+    TPoint3D operator-() const { return TPoint3D(-X, -Y, -Z); }
+    TPoint3D operator+() const { return TPoint3D(X, Y, Z); }
+
+    bool Is_Valid() const { return X > 0 || Y > 0 || Z > 0; }
+
+public:
+    T X;
+    T Y;
+    T Z;
 };
-
-
-template<typename T>
-bool operator==(const TPoint3D<T> &left, const TPoint3D<T> &right)
-{
-    return (left.X == right.X)
-        && (left.Width == right.Width)
-        && (left.Y == right.Y)
-        && (left.Height == right.Height);
-}
-
-
-template<typename T>
-bool operator!=(const TPoint3D<T> &left, const TPoint3D<T> &right)
-{
-    return (left.X != right.X)
-        && (left.Width != right.Width)
-        && (left.Y != right.Y)
-        && (left.Height != right.Height);
-}
-
-
-template<typename T>
-TPoint3D<T> operator+(const TPoint3D<T> &left, const TPoint3D<T> &right)
-{
-    TPoint3D result = left;
-    TPoint3D tmp = right;
-    result += tmp;
-    return result;
-}
-
-
-template<typename T>
-TPoint3D<T> operator-(const TPoint3D<T> &left, const TPoint3D<T> &right)
-{
-    TPoint3D result = left;
-    TPoint3D tmp = right;
-    result -= tmp;
-    return result;
-}
