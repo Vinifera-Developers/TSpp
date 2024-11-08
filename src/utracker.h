cc4cac5190e1e779dca2492a38a073dba1b84523
ZivDero <kirill.andriiashin@gmail.com>
2024-11-09 02:48:05 +0300

    Make UnitTrackerClass private members protected so that it can be extended

--- src/utracker.h
+++ src/utracker.h
@@ -27,46 +27,44 @@
  ******************************************************************************/
 #pragma once
 
-#include "always.h"
-
 
 class UnitTrackerClass
 {
-	public:
-		UnitTrackerClass(int unit_count);
-		~UnitTrackerClass();
+public:
+    UnitTrackerClass(int unit_count);
+    ~UnitTrackerClass();
 
-		void Increment_Unit_Total(int unit_type);
-		void Decrement_Unit_Total(int unit_type);
-		void Clear_Unit_Total();
+    void Increment_Unit_Total(int unit_type);
+    void Decrement_Unit_Total(int unit_type);
+    void Clear_Unit_Total();
 
-		int Get_Unit_Total(int unit_type)
-		{
-			if (UnitTotals == nullptr) {
-				return 0;
-			}
-			if (unit_type >= 0 && unit_type < UnitCount) {
-				return UnitTotals[unit_type];
-			}
-			return 0;
-		}
+    int Get_Unit_Total(int unit_type)
+    {
+        if (UnitTotals == nullptr) {
+            return 0;
+        }
+        if (unit_type >= 0 && unit_type < UnitCount) {
+            return UnitTotals[unit_type];
+        }
+        return 0;
+    }
 
-		long *Get_All_Totals();
+    long *Get_All_Totals();
 
-		void To_Network_Format();
-		void To_PC_Format();
+    void To_Network_Format();
+    void To_PC_Format();
 
-		int Get_Total_Of_All()
-		{
-			int total = 0;
-			for (int i = 0; i < UnitCount; ++i) {
-				total += UnitTotals[i];
-			}
-			return total;
-		}
+    int Get_Total_Of_All()
+    {
+        int total = 0;
+        for (int i = 0; i < UnitCount; ++i) {
+            total += UnitTotals[i];
+        }
+        return total;
+    }
 
-	private:
-		long *UnitTotals;
-		int	UnitCount;
-		int	InNetworkFormat;
+protected:
+    long *UnitTotals;
+    int	UnitCount;
+    int	InNetworkFormat;
 };
