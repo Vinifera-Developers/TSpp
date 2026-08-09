277022218cf38f014b5c14bd29ac1d7db2d36c47
Shush <almogosheriko@gmail.com>
2026-08-09 16:59:45 +0300

    Add Select_These call

--- src/tactical.h
+++ src/tactical.h
@@ -14,6 +14,7 @@
 #include "point.h"
 #include "rect.h"
 #include "ttimer.h"
+#include "object.h"
 
 
 class CellClass;
@@ -141,7 +142,7 @@ public:
     // 00616750
     // 00616760
     // 00616920
-    // 00616940
+    void Select_These(Rect& rect, void (*selection_func)(ObjectClass* obj)); // 00616940
     // 00616AA0
     // 00616C40
     // 00616C90
